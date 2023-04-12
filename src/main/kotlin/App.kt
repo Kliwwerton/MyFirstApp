4b92e05c29dd5de70f0f2c935df8be3bc0d9ca31
AStru <AStrukov-96@yandex.ru>
2023-04-12 23:17:07 +0500

    Added new tsykl while

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,11 +1,20 @@
 fun main (args: Array<String>) {
     println("Pow!")
 
-    val name = "Cormoran"
+    val name = "Cormorant"
     var x = 3
 
     x *= 10
 
     print("x is $x.")
 
+    while (x > 20) {
+        x -= 1
+        print("x is now $x")
+
+    }
+    for (i in 1..10) {
+        x += 1
+        println("x is now $x")
+    }
 }
