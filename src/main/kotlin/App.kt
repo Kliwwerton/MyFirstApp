81b22eba5b1d7d7889a3fab271452490d104b4c1
AStru <AStrukov-96@yandex.ru>
2023-04-13 22:28:03 +0500

    The loop

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,27 +1,10 @@
 fun main (args: Array<String>) {
-    println("Pow!")
-
-    val name = "Cormorant"
-    var x = 3
-
-    x *= 10
-
-    print("x is $x.")
-
-    while (x > 20) {
-        x -= 1
-        print("x is now $x")
-
-    }
-    for (i in 1..10) {
+    var x = 1
+    println("Before the loop x = $x")
+    while (x < 4) {
+        println("In the loop x = $x")
         x += 1
-        println("x is now $x")
-    }
-    if (x > 20) {
-        println("$x")
-    }
-    if (name == "Cormorant") {
-        println("name is Strike")
 
     }
+    println("After the loop x = $x")
 }
